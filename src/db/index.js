6e9fc6bd25901aed6de11c448dda5ed565a4ade4
sprint-pixel <sulovegaire321@gmail.com>
2025-09-22 07:00:51 +0545

    connect mongoDB

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+import { DB_NAME } from "../constants.js";
+
+const connectDB= async ()=>{
+    try{
+        console.log("Mongo URI →", process.env.MONGODB_URI);
+        console.log("Final URI →", `${process.env.MONGODB_URI}/${DB_NAME}`);
+
+        const connectInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n MongoDB connected!! DB host: ${connectInstance.connection.host}`);
+
+    }
+    catch(error){
+        console.log("MONGODB connection Failure: ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,36 @@
+// require ('dotenv').config({path:'./env'});
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path:'./.env'
+})
+
+//METHOD NO.1 -> to connect Database.
+/*
+import express from 'express'
+const app=express()
+//IIFE-> Immediately Invoked Funciton Execution, Syntax= `()()` the () at the end invokes the function and executes immediately.
+
+;( async ()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error",(err)=>{
+            console.log(`Error:`,err);
+            throw err;
+        });
+        app.listen(process.env.PORT,()=>{
+            console.log("Server started at port: ",process.env.PORT)
+        })
+
+    }
+    catch(err){
+        console.error("Error:",err);
+        throw err;
+    }
+})()
+*/
+
+
+// METHOD NO.2 -> importing from db folder (keeping our index.js file clean.)
+import connectDB from './db/index.js';
+connectDB()
