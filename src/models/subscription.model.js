512fa9e6b8a0c1fe314038e11b2b2c0ed5d248dd
sprint-pixel <sulovegaire321@gmail.com>
2025-11-10 14:13:04 +0545

    feat:add Subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,15 @@
+import mongoose,{Schema} from "mongoose"
+
+const subscriptionSchema= new Schema({
+    subscriber:{
+        type: Schema.Types.ObjectId, //the one who is subscribing, 
+        ref:"User"
+    },
+    channel:{
+        type:Schema.Types.ObjectId, //one to which "subscriber is subscrbing
+        ref:"User"
+    }
+},{timestamps:true})
+
+
+export const Subscription = mongoose.model("Subscription",subscriptionSchema)
