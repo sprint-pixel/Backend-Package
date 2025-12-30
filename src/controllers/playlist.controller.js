56c75420c1348f7a7491b2de0a1f0cebce0e5fd6
sprint-pixel <sulovegaire321@gmail.com>
2025-12-30 13:59:58 +0545

    feat(playlist): Implement playlist creation and fetch user playlist API

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -8,18 +8,49 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const createPlaylist = asyncHandler(async (req, res) => {
     const {name, description} = req.body
 
-    //algorithm:
-    //1. take input (name,description) from req.body
-    //2. take _id from req.user 
-    //3. In the videos array, I am confused as there can be 2 ways, either create a playlist and add video later OR only allow user to create a playlist once he has selected the video the include as the first video.let's name 1st one A and 2nd one B
-    //3.A. since we have all the required feilds and sincce video will be uploaded later in the array we will keep it empty for now and add video later on to the array, and that would sum up our controller. 
-    //3.B. TWO cases: 1. The user will be playing the video itself to add it to the playlist-> grab the _id of the video from the params,  2. We can also grab the video from the preview mode and make it our first video->  BUT the given input here dosen't allow us to take the _id of the video by the frontend. So We will go with 3.A option
-    //4. Check if the process failed to happen, if it did return 500 error, if not return succesful apiResponse with data-> newplaylist
+    if(!name || name.trim()=== ""){
+        throw new apiError(400, "Playlist Title is required ")
+    }
 
-    //TODO: create playlist
+    //the Video will be later added to the playlist
+    const createPlaylist = await Playlist.create({
+        name: name.trim(),
+        description: description ? description.trim() : "",
+        videos: [],
+        owner: req.user?._id
+    })
+
+    if(!createPlaylist){
+        throw new apiError(500, "Failed to create playlist")
+    }
+
+    return res
+    .status(201)
+    .json(new apiResponse(201, createPlaylist, "Playlist created successfully"))
 })
 
+const getUserPlaylists = asyncHandler(async (req, res) => {
+    const {userId} = req.params
+
+    if(!isValidObjectId(userId)){
+        throw new apiError(400, "Invalid user id")
+    }
+    
+    const playlists = await Playlist.aggregate([
+        {
+            $match:{
+                    owner: new mongoose.Types.ObjectId(userId), //ain't using {$exists:true} on videos as we need to return playlists with empty videos array as well
+            }
+        },
+    ])
+
+    //if we don't find any playlists, we won't want to throw an error, just return an empty array
+    return res
+    .status(200)
+    .json(new apiResponse(200, playlists? playlists : [], "User playlists fetched successfully"))
+})
 
 export {
     createPlaylist,
+    getUserPlaylists,
 }
